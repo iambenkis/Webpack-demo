3bedd736ffd0165f7b593160a41f128160466d52
benjamin kisenge <benkisenge03@gmail.com>
2022-08-10 15:50:51 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import _ from 'lodash'
+import _ from 'lodash';
+import printMe from './print.js';
+
 function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello Ben', ',', 'Here is your webpack project'], ' ');
-  
-    return element;
-}
-  
-document.body.appendChild(component());
+   const element = document.createElement('div');
+   const btn = document.createElement('button');
+
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
+   return element;
+ }
+
+ document.body.appendChild(component());
